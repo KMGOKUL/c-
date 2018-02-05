ea13cdcc0c7bfea288fa8c9a815772cb23cd0b18
KMGOKUL <35831963+KMGOKUL@users.noreply.github.com>
2018-02-05 03:23:27 -0500

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int q;
+printf(" enter factorial number ");
+if(factorial==!)
+{
+printf(" factorial number ");
+scanf("%d", &q\n\n);
+}
+else
+printf(" Invalid code");
+getch();
+}
