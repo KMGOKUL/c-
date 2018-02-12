ac57377dc27e6613a89329b52192e61357c00937
KMGOKUL <35831963+KMGOKUL@users.noreply.github.com>
2018-02-12 13:50:50 +0530

    Create jhjk.c

---
+++ jhjk.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+ 
+void main()
+{
+    int i,n;
+    scanf("%d",&n);
+    if(n>='a' && n<='z')
+    {
+        printf("no");
+    }
+ else
+ {
+     printf("yes");
+ }
+    
+}
