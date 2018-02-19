01a3028401b26aeeb6f046aa95339f3ef9db36b5
KMGOKUL <35831963+KMGOKUL@users.noreply.github.com>
2018-02-19 13:14:12 +0530

    Create gdtt.c

---
+++ gdtt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+int n;
+  printf("enter the value");
+  scanf("%d",&n);
+if(n%4==0)
+{
+printf("it is a leap year");
+}
+  else
+{
+printf("it is not a leap year");
+}
+return 0;
+}
