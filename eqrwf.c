650c84262b7e7a33acef84ac5f8cc8a20bc327ca
KMGOKUL <35831963+KMGOKUL@users.noreply.github.com>
2018-03-09 16:14:06 +0530

    Create eqrwf.c

---
+++ eqrwf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,m,s;
+printf("enter the n,m value");
+scanf("%d",&n&m);
+s=n*m;
+if(s%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+getch();
+}
