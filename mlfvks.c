c67f6f6fd662a1d9f4c437977c1131dd7d0d68c8
KMGOKUL <35831963+KMGOKUL@users.noreply.github.com>
2018-03-19 16:05:37 +0530

    Create mlfvks.c

---
+++ mlfvks.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+#include<stdio.h>
+{
+	int a,i,count=0,n;
+	scanf("%d %d",&a,&n);
+	for(i=1;i<=n;i++)
+	{
+	if(a%i==0)
+	count++;
+	}
+	if(count==2)
+	printf("%d is the prime number",a);
+	else
+	printf("%d is not a prime number",a);
+	return 0;
+	
+}
