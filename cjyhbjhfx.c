1fbc55bc0ebc2709b9988198e552a5b78fede158
KMGOKUL <35831963+KMGOKUL@users.noreply.github.com>
2018-03-21 16:17:49 +0530

    Create cjyhbjhfx.c

---
+++ cjyhbjhfx.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+  int n,m;
+  printf("Enter the N and M:");
+  scanf("%d%d",&n,&m);
+  if((n-m)%2==0)
+  printf("Even");
+  else
+  printf("Odd");
+}
