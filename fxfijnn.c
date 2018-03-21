59b022852e863221522906dc6952726f089567c4
KMGOKUL <35831963+KMGOKUL@users.noreply.github.com>
2018-03-21 16:12:38 +0530

    Create fxfijnn.c

---
+++ fxfijnn.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+hvbvbcvcfmn c
+int main(void)
+{
+int n,c=0,x=1,p=2,i;
+printf("Enter the number:");
+scanf("%d",&n);
+while(n!=1)
+{
+n=n/2;
+c++;
+}
+for(i=0;i<c+1;i++)
+{
+x=x*p;
+}
+printf("The nearest greater power of 2 is:%d",x);
+return 0;
+}
