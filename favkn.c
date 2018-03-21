0a1c1595bb69fbed4a31dc885ce042e1bc8ca734
KMGOKUL <35831963+KMGOKUL@users.noreply.github.com>
2018-03-21 16:22:00 +0530

    Create favkn.c

---
+++ favkn.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+ddvm,sd,sdvmnsd;x mvolasdf
+
+int main() {
+  int n,m;
+  printf("Enter the N and M:");
+  scanf("%d%d",&n,&m);
+  if((n-m)%2==0)
+  printf("Even");
+  else
+  printf("Odd");
+}
